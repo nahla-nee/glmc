f25741185b95738cd220f91907b4a4c02996b66e
​zee <mustafazaid2172000@gmail.com>
2018-11-24 08:11:08 -0700

    ugh

--- include/glmc_plain.h
+++ include/glmc_plain.h
@@ -3,12 +3,12 @@
 
 //non SSE version of glmc!
 //you really shouldn't use this, literally every CPU supports SSE
-#include "vector2_plain.h"
-#include "vector3_plain.h"
-#include "vector4_plain.h"
+#include "vector/vector2_plain.h"
+#include "vector/vector3_plain.h"
+#include "vector/vector4_plain.h"
 
-#include "matrix2_plain.h"
-#include "matrix3_plain.h"
-#include "matrix4_plain.h"
+#include "matrix/matrix2_plain.h"
+#include "matrix/matrix3_plain.h"
+#include "matrix/matrix4_plain.h"
 
 #endif /*GLMC_PLAIN_H*/
