ab41b397e773a2f500f2585af8b3c8936d09c304
zee220 <mustafa@zee.technology>
2019-12-12 03:09:42 -0700

    limited to 80

--- include/matrix/mat4.h
+++ include/matrix/mat4.h
@@ -13,7 +13,8 @@ extern "C"{
 #endif
 
 mat4 mat4_set(float m0, float m1, float m2, float m3, float m4, float m5,
-	float m6, float m7, float m8, float m9, float m10, float m11, float m12, float m13, float m14, float m15);
+	float m6, float m7, float m8, float m9, float m10, float m11, float m12,
+	float m13, float m14, float m15);
 mat4 mat4_set1(float x);
 mat4 mat4_add(mat4 a, mat4 b);
 mat4 mat4_sub(mat4 a, mat4 b);
